e15ec67683b95992b8e2b86c36e36d3e3c014cd9
0lgaP <perronolga@gmail.com>
2021-11-24 13:41:23 -0500

    kata 12 commited, and kata12-1 deleted?

--- kata12.js
+++ kata12.js
@@ -1,3 +1,37 @@
+function ingredientCheck(bakery, recipes) {
+    let bakeryARecipes = []
+    for (let x = 0; x < recipes.length; x++) {
+        for (let y = 0; y < bakery.length; y++) {
+            if ((recipes[x].ingredients).includes(bakery[y])) {
+                bakeryARecipes.push(recipes[x].name)
+            }
+        }
+    }
+    return bakeryARecipes
+}
+const chooseRecipe = function(bakeryA, bakeryB, recipes){
+    
+    function ingredientCheck(bakery, recipes) {
+        let bakeryARecipes = []
+        for (let x = 0; x < recipes.length; x++) {
+            for (let y = 0; y < bakery.length; y++) {
+                if ((recipes[x].ingredients).includes(bakery[y])) {
+                    bakeryARecipes.push(recipes[x].name)
+                }
+            }
+        }
+        return bakeryARecipes
+    }
+    let a = ingredientCheck(bakeryA, recipes) 
+    let b = ingredientCheck(bakeryB, recipes)
+    let result = []
+    for (let i = 0; i < a.length; i++){
+        if(a[i].includes(b))
+        result += a[i]
+    }
+    
+    return result
+}
 let bakeryA = ['saffron', 'eggs', 'tomato paste', 'coconut', 'custard'];
 let bakeryB = ['milk', 'butter', 'cream cheese'];
 let recipes = [
@@ -14,35 +48,24 @@ let recipes = [
         ingredients: ['custard', 'ground beef']
     }
 ];
-console.log(Object.entries(recipes[2]))
-const chooseRecipe = function(bakeryA, bakeryB, recipes) {
-    for (let butts in recipes){
-        // console.log(butts);
-        // console.log(recipes[butts])
-        // console.log(Object.keys(recipes))
-        console.log(Object.entries(recipes[butts]))
+
+console.log(chooseRecipe(bakeryA, bakeryB, recipes));
+
+bakeryA = ['potatoes', 'bay leaf', 'raisins'];
+bakeryB = ['red bean', 'dijon mustard', 'apples'];
+recipes = [
+    {
+        name: 'Potato Ganache',
+        ingredients: ['potatoes', 'chocolate']
+    },
+    {
+        name: 'Sweet Fish',
+        ingredients: ['anchovies', 'honey']
+    },
+    {
+        name: "Nima's Famous Dijon Raisins",
+        ingredients: ['dijon mustard', 'raisins']
     }
-}
-console.log(chooseRecipe(bakeryA, bakeryB, recipes))
+];
 
-
-// console.log(chooseRecipe(bakeryA, bakeryB, recipes));
-
-// bakeryA = ['potatoes', 'bay leaf', 'raisins'];
-// bakeryB = ['red bean', 'dijon mustard', 'apples'];
-// recipes = [
-//     {
-//         name: 'Potato Ganache',
-//         ingredients: ['potatoes', 'chocolate']
-//     },
-//     {
-//         name: 'Sweet Fish',
-//         ingredients: ['anchovies', 'honey']
-//     },
-//     {
-//         name: "Nima's Famous Dijon Raisins",
-//         ingredients: ['dijon mustard', 'raisins']
-//     }
-// ];
-
-// console.log(chooseRecipe(bakeryA, bakeryB, recipes));
+console.log(chooseRecipe(bakeryA, bakeryB, recipes));
