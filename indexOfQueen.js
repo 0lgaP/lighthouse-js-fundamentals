90dcf6903082aa78fc5bd565e05885ac998671b2
0lgaP <perronolga@gmail.com>
2021-12-08 22:16:30 +0000

    solved queen's craptastic

--- indexOfQueen.js
+++ indexOfQueen.js
@@ -1,5 +1,5 @@
-let whiteQueen = [0, 5];
-let blackQueen = [5, 0];
+let whiteQueen = [2, 3];
+let blackQueen = [1, 4];
 let generatedBoard = generateBoard(whiteQueen, blackQueen);
 
 function generateBoard(q1, q2) {
@@ -8,58 +8,29 @@ function generateBoard(q1, q2) {
     const row = []
     for (let y = 0; y < 9; y++) row.push(0);
     board.push(row);
-    
-  }
-    board[q1[0]][q1[1]] = 1;
-    board[q2[0]][q2[1]] = 1;
-    return board
+  };
+  board[q1[0]][q1[1]] = 1;
+  board[q2[0]][q2[1]] = 1;
+  return board
 }
 
+function queenThreat(board) {
+  let q1 = whiteQueen
+  let q2 = blackQueen
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      if (q1[1] === q2[1]) {
+        return true
+      } if (q1[0] === q2[0]) {
+        return true
+      } if (Math.abs(q1[1] - q2[1]) === Math.abs(q1[0] - q2[0])) {
+        return true
+      } else {
+        return false
+      }
+    }
+  }
+}
 
-  
-console.log(generatedBoard);
-// console.log(typeof(generatedBoard));
-// console.log(Object.keys(generatedBoard));
-// console.log(Object.values(generatedBoard));
-// console.log(generatedBoard.length)
-// console.log(generatedBoard[0])
-
-// function horizontal(q1, q2, board) {
-//   for (index in board){
-//     // console.log(index)
-//       for (value in index){
-//         if(1 === board[q1[1]]){
-//           console.log("queen")
-//         } else {
-//           console.log("else")
-//         }
-        
-//     }
-//   }
-  
-// }
-
-// for (i in generatedBoard){
-//   // console.log(i)
-// for (key of generatedBoard[i]){
-//   console.log(key)
-// }
-// }
-// console.log(horizontal(whiteQueen, blackQueen, generatedBoard))
-// ruminations - nth of an array can be used to search the column?
-// should i use [(generatedBoard(whiteQueen, blackQueen))[i]] to loop through the array of arrays?
-
-  // let queenThreat = function queenThreat(params) {
-  //   console.log([(generatedBoard(whiteQueen, blackQueen))[0][5]])
-     
-  //  }
-
-  //  const checkCol = function checkCol(pos, val, board) {
-     
-  //  }
-  
-
-
-
-
-// console.log(queenThreat(generatedBoard));
+console.table(generatedBoard)
+console.log(queenThreat(generatedBoard))
